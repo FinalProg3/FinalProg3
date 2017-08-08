41523d9b71df636b5fdcb910fd05d0007ede148f
wilmer vasquez de leon <cort-1998@hotmail.com>
2017-08-08 13:46:23 -0400

    Agregar archivos de proyecto.

---
+++ FinalProgIII/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProgIII.Startup))]
+namespace FinalProgIII
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
