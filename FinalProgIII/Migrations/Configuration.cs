4af3d8eb8a15af20547f99fdbe3ceb231528de80
Arian manuel Garcia <the-master-aaa-@hotmail.com>
2017-08-08 14:21:08 -0400

    subio

--- FinalProgIII/Migrations/Configuration.cs
+++ FinalProgIII/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace FinalProgIII.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(FinalProgIII.Models.ApplicationDbContext context)
